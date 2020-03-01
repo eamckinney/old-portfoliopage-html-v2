4445e920b92f632c9a3cbbb4ff5d8bb4aedf10a8
Emily McKinney <emily.a.mckinney@gmail.com>
2020-03-01 11:36:11 -0600

    Smooth scrolling, font change

--- js/script.js
+++ js/script.js
@@ -1,14 +1,44 @@
+// When navbar item is clicked, go 66 pixels above the target
 window.addEventListener("hashchange", function () {
-    window.scrollTo(window.scrollX, window.scrollY - 66);
+        
+    //window.scrollTo(window.scrollX, window.scrollY - 60);
+        //behavior: 'smooth'
+    //});
 });
 
 $(document).ready(function () {
+    
+    // Changes which button in the navbar is active when clicked
     $('.navbar li a').click(function(e) {
 
-        $('.navbar li.active').removeClass('active');
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            e.preventDefault();
+        
+            // Store hash
+            var hash = this.hash;
 
-        var $parent = $(this).parent();
-        $parent.addClass('active');
-        //e.preventDefault();
+            // Add smooth page scroll
+            $('html, body').animate({
+                scrollTop: ($(hash).offset().top - 54)
+            }, 500, function(){
+        
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                //window.location.hash = hash;
+            });
+
+            $('.navbar li.active').removeClass('active');
+
+            var $parent = $(this).parent();
+            $parent.addClass('active');
+
+        } // End if
+
+
     });
+
+
+    
+
 });
